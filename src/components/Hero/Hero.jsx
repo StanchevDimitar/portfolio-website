d56e4fbd73f57ba5a1789026ec60aa28eaa72252
Snezhana Petlova <snpetlova@gmail.com>
2025-05-10 11:25:17 +0300

    change img

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import heroImage from "../../../assets/hero/heroImage.png";
+import heroImage from "../../../assets/hero/heroImage.jpg";
 
 export const Hero = () => {
   return (
